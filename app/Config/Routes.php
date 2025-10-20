1a83feb85c10b8b0fe540781f9b4f04749bd0904
FGFERNAN <fgfernan2508@gmail.com>
2025-10-20 16:23:30 -0500

    Configuración de Proyecto
    Se define y crea estructura de carpetas y archivos para el proyecto.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+
+// Autenticación
+$routes->get('/login', 'Auth::login');
+$routes->post('/login', 'Auth::attemptLogin');
+$routes->get('/register', 'Auth::register');
